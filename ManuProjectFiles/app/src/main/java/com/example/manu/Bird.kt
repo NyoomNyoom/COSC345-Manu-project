62744e7c772285fe8b838588dca5fc5bc08ef5e1
NyoomNyoom <93577613+NyoomNyoom@users.noreply.github.com>
2022-08-05 13:27:16 +1200

    Continuing work on bird class and adapter

--- ManuProjectFiles/app/src/main/java/com/example/manu/Bird.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/Bird.kt
@@ -1,6 +1,40 @@
 /* Jackson North 03/08/2022 */
 package com.example.manu
 
-data class bird(val Name: String, var fileName: String, var altName: String, var infoFile: String) {
 
+
+data class Bird(val Name: String, val fileName: String, val altName: String, val infoFile: String) {
+    private var nameOfBird = Name
+    //private var birdFile
+    private var alternativeName = altName
+    private var birdInfoFile = infoFile
+
+    fun birdName(): String{
+        return nameOfBird
+    }
+
+    /*
+    fun getFile(fileType : String): File{
+        try {
+            val inputStream = assets.open("birdTest.txt")
+            val size: Int = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer).toString()
+
+            string = String(buffer)
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+
+    }
+    */
+
+
+    fun getAltName(){
+
+    }
+
+    fun getInfoFile(){
+
+    }
 }
--- ManuProjectFiles/app/src/main/java/com/example/manu/BirdAdapter.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/BirdAdapter.kt
@@ -3,17 +3,31 @@ package com.example.manu
 
 /* Class takes an empty list in of type bird, and then the function create quiz to be called which
 will create the quiz. */
-class BirdAdapter(val questionList : MutableList<bird>) {
+class BirdAdapter(val questionList : MutableList<Bird>) {
 
     //Creates a quiz using the amount of questions and type that the creator wants.
     fun createQuiz(questionAmount: Int, questionType: Int){
         for(i in 1..questionAmount){
-            questionList[i] = randomBird(questionType)
+            //questionList[i] = randomBird(questionType)
         }
     }
 
     //function that finds a random bird for the question type.
-    fun randomBird(questionType : Int){
+    /*
+    private fun randomBird(questionType : Int): Bird {
+
+    }
+    */
+
+    //function checks if the bird that is given is a valid bird for the question.
+    private fun isValid(questionType : Int, bird : Bird){
+        if(questionType == 1){
+
+        }
+    }
+
+    //creates a random bird from the given bird number and question type.
+    private fun createBird(questionType: Int, birdNumber : Int){
 
     }
 }
