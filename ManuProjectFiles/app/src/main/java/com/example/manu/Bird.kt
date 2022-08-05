7ec82747610c89d0303383945770bbe00cbe25da
NyoomNyoom <93577613+NyoomNyoom@users.noreply.github.com>
2022-08-05 14:57:03 +1200

    continuing work on the bird class and adapter

--- ManuProjectFiles/app/src/main/java/com/example/manu/Bird.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/Bird.kt
@@ -1,11 +1,19 @@
 /* Jackson North 03/08/2022 */
 package com.example.manu
 
-
+/*
+* Need to change the constructor to be a constructor with only a number inputed which refers to a
+* number from a csv file that has all the information currently in the folder name and then saves
+* the information to a string which will then be able to be called out by the functions in
+* birdAdapter.
+*
+* NEED TO HAVE A LIST THAT HAS 4 BIRDS (1 CORRECT 3 WRONG) WITH THE CORRECT INDEX AS A SECOND NUMBER
+* RETURNED.
+*/
 
 data class Bird(val Name: String, val fileName: String, val altName: String, val infoFile: String) {
     private var nameOfBird = Name
-    //private var birdFile
+    private var birdFile = fileName
     private var alternativeName = altName
     private var birdInfoFile = infoFile
 
@@ -13,22 +21,9 @@ data class Bird(val Name: String, val fileName: String, val altName: String, val
         return nameOfBird
     }
 
-    /*
-    fun getFile(fileType : String): File{
-        try {
-            val inputStream = assets.open("birdTest.txt")
-            val size: Int = inputStream.available()
-            val buffer = ByteArray(size)
-            inputStream.read(buffer).toString()
-
-            string = String(buffer)
-        }catch (e: IOException){
-            e.printStackTrace()
-        }
+    fun getFile(fileType : String): String{
 
     }
-    */
-
 
     fun getAltName(){
 
--- ManuProjectFiles/app/src/main/java/com/example/manu/BirdAdapter.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/BirdAdapter.kt
@@ -8,21 +8,19 @@ class BirdAdapter(val questionList : MutableList<Bird>) {
     //Creates a quiz using the amount of questions and type that the creator wants.
     fun createQuiz(questionAmount: Int, questionType: Int){
         for(i in 1..questionAmount){
-            //questionList[i] = randomBird(questionType)
+            questionList[i] = randomBird(questionType)
         }
     }
 
     //function that finds a random bird for the question type.
-    /*
     private fun randomBird(questionType : Int): Bird {
 
     }
-    */
 
     //function checks if the bird that is given is a valid bird for the question.
-    private fun isValid(questionType : Int, bird : Bird){
+    private fun isValid(questionType :7 Int, bird : Bird){
         if(questionType == 1){
-
+            if(bird.)
         }
     }
 
