bedf6519a6bace62cdf9c86d4b5c189a9b58bfa0
NyoomNyoom <93577613+NyoomNyoom@users.noreply.github.com>
2022-08-11 10:16:03 +1200

    Added script and content view switching code
    Commented the code which allows content view and script switching. Please read it and apply it for your contentView's.
    
    Co-Authored-By: Daniel-Ian-Robinson <92424879+Daniel-Ian-Robinson@users.noreply.github.com>

--- ManuProjectFiles/app/src/main/java/com/example/manu/HelloWorld.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/HelloWorld.kt
@@ -1,15 +1,20 @@
+/**
+ * This script is an exact copy of a view controlling Kotlin class (like the default MainActivity.kt
+ * script class). The lines with comments saying "ADDED: " are in addition to the default code.
+ */
+
 package com.example.manu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.util.Log  // ADDED: Allows you to print debugging statements.
 
 class HelloWorld : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.info_graphic_activity)
-        Log.d("HelloWorld", "Well, that worked!")
+        setContentView(R.layout.activity_main)  // Change "activity_main" to your desired view.
+        Log.d("HelloWorld", "Well, that worked!")  // ADDED: Print text to prove it works.
     }
 
 }
--- ManuProjectFiles/app/src/main/java/com/example/manu/MainActivity.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/MainActivity.kt
@@ -7,11 +7,14 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+
+        /**
+         * Change "HelloWorld" below to the class you wish to instantiate when the app is launched.
+         * Make sure your chosen class specifies which view to use. See HelloWorld.kt for an
+         * example.
+         */
         val intent = Intent(this, HelloWorld::class.java)
-        startActivity(intent)
-        finish()
-
-
+        startActivity(intent)  // Launch the class script.
+        finish()  // Terminate this class script.
     }
 }
