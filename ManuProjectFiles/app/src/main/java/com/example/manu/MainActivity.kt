d7d7c50daba792f47ea73bb3c1bbeb3dcb6da7c4
Daniel-Ian-Robinson <daniel.robinson.21.dr@gmail.com>
2022-08-14 22:34:26 +1200

    Fully documented MainActivity.kt

--- ManuProjectFiles/app/src/main/java/com/example/manu/MainActivity.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/MainActivity.kt
@@ -4,7 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * The very first script to run when the app is launched. This script does not load a content view. Instead, it runs a
+ * script which should load its own content view.
+ */
 class MainActivity : AppCompatActivity() {
+    /**
+     * Is run when this class is instantiated. It will load the specified script.
+     *
+     * @param Bundle Saves information between separate loads of this activity view.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
