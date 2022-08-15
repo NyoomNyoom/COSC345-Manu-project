880fa0b306f83d559fedec63d539f2119b690c24
wrushton0 <109555679+wrushton0@users.noreply.github.com>
2022-08-15 22:21:50 +1200

    Update InfoGraphicActivity.kt

--- ManuProjectFiles/app/src/main/java/com/example/manu/InfoGraphicActivity.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/InfoGraphicActivity.kt
@@ -2,20 +2,30 @@
 
 package com.example.manu
 
+import android.content.Intent
 import android.graphics.Color
-import android.util.Log
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
 import kotlinx.android.synthetic.main.activity_menu.*
+
 
 /* Honestly I don't know yet...
  */
 class InfoGraphicActivity : AppCompatActivity() {
+
+    private lateinit var gestureDetector: GestureDetectorCompat
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.info_graphic_activity)
+
+        gestureDetector = GestureDetectorCompat(this, GestureListener())
 
         val allButtons: ArrayList<View>
         allButtons = (findViewById<View>(R.id.scrollView1) as ScrollView).touchables // Size is 48 (birds 47 + 1)
@@ -25,10 +35,64 @@ class InfoGraphicActivity : AppCompatActivity() {
         for(i in 0 until size){
             //Get bird image and add it to button
             allButtons[i].setBackgroundColor(Color.RED) // Sets the backgroundC
-            allButtons[i].setOnClickListener{
+            allButtons[i].setOnClickListener {
                 // Create a pop up window and pass it the text information
-                finish()
             }
         }
     }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (gestureDetector.onTouchEvent(event)) {
+            true
+        }
+        else{
+            super.onTouchEvent(event)
+        }
+    }
+
+    inner class GestureListener : GestureDetector.SimpleOnGestureListener()
+    {
+
+        private val SWIPE_THRESHOLD = 100
+        private val SWIPE_VELOCITY_THRESHOLD = 100
+
+        override fun onFling(downEvent: MotionEvent?, moveEvent: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+            var diffX = moveEvent?.x?.minus(downEvent!!.x) ?: 0.0F
+            var diffY = moveEvent?.y?.minus(downEvent!!.y) ?: 0.0F
+
+            return if(Math.abs(diffX) > Math.abs(diffY)) {
+                // this is a left or right swipe
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0 ){
+                        // right swipe
+                        this@InfoGraphicActivity.onSwipeRight()
+                    } else {
+                        // left swipe
+                    }
+                    true
+                } else {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            } else {
+                // this is a top or bottom swipe
+                if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        // swipe top
+                    } else {
+                        // swipe bottom
+                    }
+                    true
+                } else {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            }
+        }
+    }
+
+    private fun onSwipeRight() {
+        var intent = Intent(this, Pop::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
