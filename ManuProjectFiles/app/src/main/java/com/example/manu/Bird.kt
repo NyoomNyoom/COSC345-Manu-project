240084d2b303896d43d921cc5b407ed768897614
NyoomNyoom <jacksonnorth1275@gmail.com>
2022-08-17 10:58:45 +1200

    Documenting bird.kt

--- ManuProjectFiles/app/src/main/java/com/example/manu/Bird.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/Bird.kt
@@ -5,11 +5,15 @@ import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
 
-/*
-* NEED TO HAVE A LIST THAT HAS 4 BIRDS (1 CORRECT 3 WRONG) WITH THE CORRECT INDEX AS A SECOND NUMBER
-* RETURNED.
-*/
-
+/**
+ * @author Jackson North
+ * A class which holds all the values needed to keep track of a bird for the given question. It takes in a random
+ * number from BirdAdapter.kt and then searches through a list of birds to find that number and then checks
+ * if the bird is valid for the quiz, and if it is, will update the values of the bird so we have all the needed
+ * values that were stored in the information list.
+ *
+ * @param birdNumIn the index of a bird in the list of birds we have on file.
+ */
 data class Bird(val birdNumIn: Int) {
     private var nameOfBird = ""
     private var birdFilePic = ""
@@ -18,8 +22,13 @@ data class Bird(val birdNumIn: Int) {
     private var birdInfoFile = ""
     private var birdNum = birdNumIn
 
-
+    /**
+     * A function that searches the bird-data.csv file which is a list of birds to find the bird that was
+     * inputted into the class constructor and then add the values of the bird to the private variables.
+     */
     fun updateValues(){
+
+
         /*
         var birdNumber = birdNum
         var birdInfo: MutableList<String> = mutableListOf()
@@ -47,12 +56,20 @@ data class Bird(val birdNumIn: Int) {
          */
     }
 
-
+    /**
+     * A function that returns the name of a bird from the private variable.
+     *
+     * @return nameOfBird: String, the name of the bird.
+     */
     fun getBirdName(): String{
         return nameOfBird
     }
 
-    //returns the file name for referencing in the question.
+    /**
+     * A function that returns the file of a bird from a private variable depending on which question is asked.
+     *
+     * @return output: String, the correct file for the bird.
+     */
     fun getFile(questionType: Int): String{
         var output = ""
 
@@ -66,18 +83,34 @@ data class Bird(val birdNumIn: Int) {
         return output
     }
 
+    /**
+     * A function that returns the alternative name of a bird from the private variable.
+     *
+     * @return alternativeName: String, the name of the bird.
+     */
     fun getAltName(): String{
         return alternativeName
     }
 
-    fun getInfoFile(): String{
+    private fun getInfoFile(): String{
         return birdInfoFile
     }
 
+    /**
+     * A function that returns the index that the bird is in the list.
+     *
+     * @return birdNum: Int, the index of the bird.
+     */
     fun getBirdNum(): Int{
         return birdNum
     }
 
+    /**
+     * Overriding the default to string method. Which returns all the birds data in a line separated by
+     * commas.
+     *
+     * @return nameOfBird, AlternativeName, birdNum, birdFilePic, birdFileSong, birdInfoFile.
+     */
     override fun toString(): String {
         return "$nameOfBird, $alternativeName, $birdNum, $birdFilePic, $birdFileSong, $birdInfoFile"
     }
