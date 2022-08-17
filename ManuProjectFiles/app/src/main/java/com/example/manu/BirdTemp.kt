cad04d9ae19cd18eb482046520092d85e6cd32e4
Daniel-Ian-Robinson <daniel.robinson.21.dr@gmail.com>
2022-08-18 11:27:46 +1200

    BirdTemp just stores photo name pairs

--- ManuProjectFiles/app/src/main/java/com/example/manu/BirdTemp.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/BirdTemp.kt
@@ -1,11 +1,17 @@
 package com.example.manu
 
-class BirdTemp(birdName: String, photoResourceId: Int, soundResourceId: Int, māoriName: String, englishName: String) {
+/**
+ * Stores information about a bird.
+ *
+ * @param birdName The bird's primary name.
+ * @param photoResourceId The photo's resource ID (a drawable resource).
+ */
+class BirdTemp(birdName: String, photoResourceId: Int/*, soundResourceId: Int, māoriName: String, englishName: String*/) {
 
     private val birdName: String = birdName
     private val photoResourceId: Int = photoResourceId
-    private val soundResourceId: Int = soundResourceId
-    private val māoriName: String = māoriName
-    private val englishName: String = englishName
+    //private val soundResourceId: Int = soundResourceId
+    //val māoriName: String = māoriName
+    //private val englishName: String = englishName
 
 }
