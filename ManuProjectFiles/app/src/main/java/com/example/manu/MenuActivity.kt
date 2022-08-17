4bff9cb3cde00dcbf52240d16750d228ec54e3e3
mgmccane <101371372+mgmccane@users.noreply.github.com>
2022-08-18 09:49:43 +1200

    Swiping to infographic from menu

--- ManuProjectFiles/app/src/main/java/com/example/manu/MenuActivity.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/MenuActivity.kt
@@ -3,12 +3,17 @@ package com.example.manu
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_menu.*
+import androidx.core.view.GestureDetectorCompat
 
 /**
  * Runs and displays the main menu.
  */
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var gestureDetector: GestureDetectorCompat
 
     /**
      * This is run when the class is instantiated. Hands control to either the infographic screen
@@ -22,6 +27,9 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)  // Set the layout to the menu layout.
         //val button = findViewById<View>(R.id.button1)
         // When this button is pressed, load the quiz, and exit from this script.
+
+        gestureDetector = GestureDetectorCompat(this, GestureListener())
+
         button1.setOnClickListener {
             val intent = Intent(this, QuizActivity::class.java)
             startActivity(intent)
@@ -32,6 +40,61 @@ class MenuActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (gestureDetector.onTouchEvent(event)) {
+            true
+        }
+        else{
+            super.onTouchEvent(event)
+        }
+    }
+
+    inner class GestureListener : GestureDetector.SimpleOnGestureListener()
+    {
+
+        private val SWIPE_THRESHOLD = 100
+        private val SWIPE_VELOCITY_THRESHOLD = 100
+
+        override fun onFling(downEvent: MotionEvent?, moveEvent: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+            var diffX = moveEvent?.x?.minus(downEvent!!.x) ?: 0.0F
+            var diffY = moveEvent?.y?.minus(downEvent!!.y) ?: 0.0F
+
+            return if(Math.abs(diffX) > Math.abs(diffY)) {
+                // this is a left or right swipe
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0 ){
+                        // right swipe
+                        this@MenuActivity.onSwipeRight()
+                    } else {
+                        // left swipe
+                    }
+                    true
+                } else {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            } else {
+                // this is a top or bottom swipe
+                if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        // swipe top
+                    } else {
+                        // swipe bottom
+                    }
+                    true
+                } else {
+                    super.onFling(downEvent, moveEvent, velocityX, velocityY)
+                }
+            }
+        }
+    }
+
+    private fun onSwipeRight() {
+        var intent = Intent(this, InfoGraphicActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     /**
