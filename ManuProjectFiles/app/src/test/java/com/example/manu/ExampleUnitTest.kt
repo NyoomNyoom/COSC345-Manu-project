fe89095842da441f4709fff7038d335bcad89b90
NyoomNyoom <93577613+NyoomNyoom@users.noreply.github.com>
2022-08-18 09:45:59 +1200

    Adding a shitty test

--- ManuProjectFiles/app/src/test/java/com/example/manu/ExampleUnitTest.kt
+++ ManuProjectFiles/app/src/test/java/com/example/manu/ExampleUnitTest.kt
@@ -11,6 +11,11 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
 
+    @Test
+    fun addition_isCorrect(){
+        assertEquals(4, 2+2)
+    }
+
     /*
     @Test
     fun isValid_isCorrect(){
