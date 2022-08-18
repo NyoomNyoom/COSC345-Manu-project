b8ac968ecc6f5afe790ed2fdbad5d49a43820856
NyoomNyoom <93577613+NyoomNyoom@users.noreply.github.com>
2022-08-19 11:49:53 +1200

    I dunno what I did

--- ManuProjectFiles/app/src/main/java/com/example/manu/QuestionHardCoded.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/QuestionHardCoded.kt
@@ -3,9 +3,9 @@ package com.example.manu
 import kotlin.random.Random
 
 data class QuestionHardCoded(val questionNum: Int){
-    val questionList: MutableList<Question> = mutableListOf()
+    private val questionList: MutableList<Question> = mutableListOf()
 
-    fun createQuiz(){
+    fun createQuiz(): MutableList<Question>{
         var i = 0
         while(i <= questionNum-1 && questionNum <= 10){
             questionList.add(Question(Bird(i),1))
@@ -18,6 +18,8 @@ data class QuestionHardCoded(val questionNum: Int){
         }else {
 
         }
+
+        return questionList
     }
 
 }
