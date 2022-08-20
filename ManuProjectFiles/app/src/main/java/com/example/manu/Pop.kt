511d441e277d9d190d4e0189c4f7f07cd3e738eb
Daniel-Ian-Robinson <daniel.robinson.21.dr@gmail.com>
2022-08-20 12:51:09 +1200

    Animated "yes" "no" buttons
    Only displays the first frame of the animation, the cut to a smaller button. However, when we have smoother transitions between layouts, you will see the button rise back to its normal size in the rest of the animation.

--- ManuProjectFiles/app/src/main/java/com/example/manu/Pop.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/Pop.kt
@@ -8,22 +8,26 @@ import android.util.DisplayMetrics
 import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.core.view.WindowCompat
 import kotlinx.android.synthetic.main.popup_window.*
 
 class Pop : Activity() {
 
+    private lateinit var buttonPress: Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.popup_window)
+
+        buttonPress = AnimationUtils.loadAnimation(this, R.anim.button_press)
 
         // Hide the navigation and status bars.
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         WindowCompat.setDecorFitsSystemWindows(window, true)  // Places the layout outside the navbar and status bar.
-
-
 
         //val dialog = Dialog(this)
         //dialog.setContentView(R.layout.popup_window)
@@ -51,12 +55,14 @@ class Pop : Activity() {
     }
 
     private fun returnToMenu() {
+        btn_yes.startAnimation(buttonPress)
         var intent = Intent(this, MenuActivity::class.java)
         startActivity(intent)
         finish()
     }
 
     private fun closePopup() {
+        btn_no.startAnimation(buttonPress)
         finish()
     }
 
