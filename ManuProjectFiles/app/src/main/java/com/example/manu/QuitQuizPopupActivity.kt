98dce8d270b1a3bc8e693526879d13ed3edd2c10
mgmccane <101371372+mgmccane@users.noreply.github.com>
2022-09-13 16:21:39 +1200

    Added functionality to quit app from menu

---
+++ ManuProjectFiles/app/src/main/java/com/example/manu/QuitQuizPopupActivity.kt
@@ -0,0 +1,69 @@
+/**
+ * @author Daniel Robinson, Madeline McCane
+ */
+
+package com.example.manu
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.view.WindowCompat
+import kotlinx.android.synthetic.main.return_to_menu_popup.*
+
+/**
+ * Controls the "Are you sure you want to quit?" popup that shows when you press the 'X' button during the quiz.
+ */
+class QuitQuizPopupActivity : Activity() {
+
+    private lateinit var buttonPress: Animation
+
+    /**
+     * This is run when the class is instantiated. It sets up the "Quit the Quiz?" layout.
+     *
+     * @param Bundle Saves information between separate loads of this activity.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.quit_quiz_popup)
+
+        buttonPress = AnimationUtils.loadAnimation(this, R.anim.button_press)
+
+        // Hide the navigation and status bars.
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        WindowCompat.setDecorFitsSystemWindows(window, true)  // Places the layout outside the navbar and status bar.
+
+        btn_yes.setOnClickListener { quit() }
+        btn_no.setOnClickListener { closePopup() }
+    }
+
+    /**
+     * Animates the button press, closes the app.
+     */
+    private fun quit() {
+        btn_yes.startAnimation(buttonPress)
+        finishAffinity()
+    }
+
+    /**
+     * Animates the button press and closes this popup.
+     */
+    private fun closePopup() {
+        btn_no.startAnimation(buttonPress)
+        finish()
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)  // Must occur after we close the popup.
+    }
+
+    /**
+     * Disables any effects of the Android system's back button.
+     */
+    override fun onBackPressed() {
+        return
+    }
+
+}
