00a066e533cd2a791025fad6b2463fe4d48b8786
Daniel-Ian-Robinson <daniel.robinson.21.dr@gmail.com>
2022-10-05 12:37:34 +1300

    Documented AudioManager.kt

--- ManuProjectFiles/app/src/main/java/com/example/manu/AudioManager.kt
+++ ManuProjectFiles/app/src/main/java/com/example/manu/AudioManager.kt
@@ -3,12 +3,24 @@ package com.example.manu
 import android.content.Context
 import android.media.MediaPlayer
 
+/**
+ * Manages the audio for this app. It plays, on loop, up to one audio track at a time.
+ */
 class AudioManager {
 
+    /**
+     * Manages the audio for this app. It plays, on loop, up to one audio track at a time.
+     */
     companion object {
 
         var mediaPlayer = MediaPlayer()
 
+        /**
+         * Plays, on loop, the audio resource provided.
+         *
+         * @param context The calling method's context (e.g., an AppCompatActivity).
+         * @param audioResourceId The desired audio resource's resource ID.
+         */
         fun playAudio(context: Context, audioResourceId: Int) {
             mediaPlayer.pause()
             mediaPlayer = MediaPlayer.create(context, audioResourceId)
@@ -16,14 +28,24 @@ class AudioManager {
             mediaPlayer.start()
         }
 
+        /**
+         * Resumes playing the current audio resource.
+         */
         fun resumeAudio() {
             mediaPlayer.start()
         }
 
+        /**
+         * Pauses the current playing audio track such that it can be resumed from the same place in the track.
+         */
         fun pauseAudio() {
             mediaPlayer.pause()
         }
 
+        /**
+         * Stops playing the current audio track. Calling this function means you cannot use resumeAudio on the current
+         * track, instead you must call playAudio again.
+         */
         fun stopAudio() {
             mediaPlayer.stop()
         }
